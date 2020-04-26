62f51e37088e236c8bf164ae92c9a6f054987999
Michelle Wong <michellewong793@gmail.com>
2020-04-26 15:53:36 -0700

    add resume and kawaiiii

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,6 @@ export default function Index() {
             <div style={Styles.content}>
                 <h1 style={Styles.header}>Hi, I'm Michelle.</h1>
                 <p style={Styles.body}>i am a frontend developer. this is my website built with React and Next.js. welcome!  </p>
-
             </div>
 
         </div>
