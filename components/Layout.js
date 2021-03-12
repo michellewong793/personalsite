00ba7941ec57df107afdddb4fa0bed9036f58dcb
michellewong793 <michellewong793@gmail.com>
2021-03-12 13:03:54 -0800

    add new text to index

--- components/Layout.js
+++ components/Layout.js
@@ -1,62 +1,74 @@
-
-import Link from 'next/link';
-import Styles from './Theme';
-import Spacer from './Spacer';
+import Link from "next/link";
+import Spacer from "./Spacer";
 
 const linkStyle = {
-    marginRight: 15,
-    fontFamily: 'Spartan',
-    textDecoration: 'none',
-    color: 'black',
+  marginRight: 15,
+  fontFamily: "Spartan",
+  textDecoration: "none",
+  color: "black",
 };
 
 const header = {
-    position: 'fixed',
-    padding: '1rem',
-    top: 0,
-    left: 0,
-    width: '100%',
-    height: '1rem',
-    backgroundColor: 'pink',
-    color: 'white',
-}
+  position: "fixed",
+  padding: "1rem",
+  top: 0,
+  left: 0,
+  width: "100%",
+  height: "1rem",
+  backgroundColor: "pink",
+  color: "white",
+};
 
 const footer = {
-    position: 'fixed',
-    padding: '1rem',
-    left: 0,
-    bottom: 0,
-    width: '100%',
-    height: '1rem',
-    backgroundColor: 'pink',
-    color: 'white',
-    display: 'flex',
-    justifyContent: 'center',
-}
+  position: "fixed",
+  padding: "1rem",
+  left: 0,
+  bottom: 0,
+  width: "100%",
+  height: "1rem",
+  backgroundColor: "pink",
+  color: "white",
+  display: "flex",
+  justifyContent: "center",
+};
 
 const Header = () => (
-    <div style={header}>
-        <Link href="/">
-            <a style={linkStyle}>Home</a>
-        </Link>
-        <a style={linkStyle} href="/resume.pdf">Resume</a>
-        <Link href="/poems">
-            <a style={linkStyle}>Poetry</a>
-        </Link>
-        <Link href="/mentor">
-            <a style={linkStyle}>Mentor</a>
-        </Link>
-    </div>
+  <div style={header}>
+    <Link href="/">
+      <a style={linkStyle}>Home</a>
+    </Link>
+    <a style={linkStyle} href="/resume.pdf">
+      Resume
+    </a>
+    <Link href="/poems">
+      <a style={linkStyle}>Poetry</a>
+    </Link>
+    <Link href="/mentor">
+      <a style={linkStyle}>Mentor</a>
+    </Link>
+  </div>
 );
 
 const Layout = () => {
-    return <> <Header /> <Footer /></>
-}
+  return (
+    <>
+      {" "}
+      <Header /> <Footer />
+    </>
+  );
+};
 
-const Footer = () => (<div style={footer}>
-    <a style={linkStyle} href="https://github.com/michellewong793"> Github </a>
-    <a style={linkStyle} href="https://linkedin.com/in/michellewong793"> LinkedIn </a>
-</div>);
+const Footer = () => (
+  <div style={footer}>
+    <a style={linkStyle} href="https://github.com/michellewong793">
+      {" "}
+      Github{" "}
+    </a>
+    <a style={linkStyle} href="https://linkedin.com/in/michellewong793">
+      {" "}
+      LinkedIn{" "}
+    </a>
+  </div>
+);
 
 export default Layout;
-
--- pages/index.js
+++ pages/index.js
@@ -1,38 +1,98 @@
-import Layout from '../components/Layout';
-import Styles from '../components/Theme';
-import Spacer from '../components/Spacer';
-import Link from 'next/link';
-
+import Layout from "../components/Layout";
+import Styles from "../components/Theme";
+import Spacer from "../components/Spacer";
 
 export default function Index() {
-    return (
-        <div>
-            <Layout />
-            <div style={Styles.content}>
-                <h1 style={Styles.header}>Hi, I'm Michelle Marsha Wong.</h1>
-                <h2 style={Styles.body}>i code.</h2>
-                <p style={Styles.body}>i work at cLabs building <a href="https://valoraapp.com" style={Styles.link} >Valora</a>.</p>
-                <p style={Styles.body}>i used to work at <a href="https://minaprotocol.com" style={Styles.link} > Mina Protocol, </a> and i helped built the site itself.  </p>
-                <p style={Styles.body}>check out my <a href="https://github.com/michellewong793" style={Styles.link} >Github</a>, if you're curious.</p>
-                <Spacer height={2} />
-
-                <h2 style={Styles.body}>i am an <a href="https://instagram.com/brain_to_ink" style={Styles.link}> artist. </a>
-                </h2>
-
-                <a href="/mentor" style={Styles.link}> "Mentor" - a submission made for Be Still Media </a>
-                <Spacer height={2} />
-
-                <h2 style={Styles.body}>i am a writer and poet. </h2>
-
-                <Spacer height={0.2} />
-
-                <p style={Styles.body}>The Sophian: <a style={Styles.link} href="https://thesophian.com/2018-10-25-poc-athlete-movement-demands-diversity-and-inclusion-in-athletics/" >POC ATHLETE MOVEMENT DEMANDS DIVERSITY AND INCLUSION IN ATHLETICS</a> </p>
-                <p style={Styles.body}>The Sophian: <a style={Styles.link} href="https://thesophian.com/2019-2-7-eradicating-smith-culture-i-smile-at-smith-students-i-dont-know-and-i-like-it/">FINDING JOY IN THE EVERYDAY - ERADICATING SMITH CULTURE</a> </p>
-                <p style={Styles.body}>The Sophian Editorial Cartoon: <a style={Styles.link} href="https://thesophian.com/2018-11-15-is-this-healing/">WHAT WORLD ARE YOU IN, SMITH? </a> </p>
-
-                <Spacer height={2} />
-                <img src="/firstGen.png" style={Styles.firstGenPromoCard}></img>
-            </div>
-        </div >
-    );
+  return (
+    <div>
+      <Layout />
+      <div style={Styles.content}>
+        <h1 style={Styles.header}>Hi, I'm Miche.</h1>
+        <h2 style={Styles.body}>i code.</h2>
+        <p style={Styles.body}>
+          i work at cLabs building{" "}
+          <a href="https://valoraapp.com" style={Styles.link}>
+            Valora
+          </a>
+          .
+        </p>
+        <p style={Styles.body}>
+          i used to work at{" "}
+          <a href="https://minaprotocol.com" style={Styles.link}>
+            {" "}
+            Mina Protocol,{" "}
+          </a>{" "}
+          and i helped built the site itself.{" "}
+        </p>
+        <p style={Styles.body}>
+          check out my{" "}
+          <a href="https://github.com/michellewong793" style={Styles.link}>
+            Github
+          </a>
+          , if you're curious.
+        </p>
+        <Spacer height={2} />
+        <h2 style={Styles.body}>
+          i am an{" "}
+          <a href="https://instagram.com/brain_to_ink" style={Styles.link}>
+            {" "}
+            artist.{" "}
+          </a>
+        </h2>
+        <h2 style={Styles.body}>
+          i made a{" "}
+          <a
+            href="https://www.dropbox.com/h?preview=confessions_01.pdf"
+            style={Styles.link}
+          >
+            {" "}
+            magazine.{" "}
+          </a>
+        </h2>
+        <h2 style={Styles.body}>
+          i am the host of{" "}
+          <a
+            href="https://open.spotify.com/show/09FJCjGxjionhSWXNcOyFN?si=crE6gyZaRuKalq37PWoTBQ"
+            style={Styles.link}
+          >
+            {" "}
+            The First Gen podcast.{" "}
+          </a>
+        </h2>
+        <Spacer height={2} />
+        <h2 style={Styles.body}>
+          i write a lot of things, and i used to be really into journalism and
+          write for my school papers.{" "}
+        </h2>
+        <Spacer height={0.2} />
+        <p style={Styles.body}>
+          The Sophian:{" "}
+          <a
+            style={Styles.link}
+            href="https://thesophian.com/2018-10-25-poc-athlete-movement-demands-diversity-and-inclusion-in-athletics/"
+          >
+            POC ATHLETE MOVEMENT DEMANDS DIVERSITY AND INCLUSION IN ATHLETICS
+          </a>{" "}
+        </p>
+        <p style={Styles.body}>
+          The Sophian:{" "}
+          <a
+            style={Styles.link}
+            href="https://thesophian.com/2019-2-7-eradicating-smith-culture-i-smile-at-smith-students-i-dont-know-and-i-like-it/"
+          >
+            FINDING JOY IN THE EVERYDAY - ERADICATING SMITH CULTURE
+          </a>{" "}
+        </p>
+        <p style={Styles.body}>
+          The Sophian Editorial Cartoon:{" "}
+          <a
+            style={Styles.link}
+            href="https://thesophian.com/2018-11-15-is-this-healing/"
+          >
+            WHAT WORLD ARE YOU IN, SMITH?{" "}
+          </a>{" "}
+        </p>
+      </div>
+    </div>
+  );
 }
