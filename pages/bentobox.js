dfe63e91da297374a48af7f654a7012af32615ec
michellewong793 <michellewong793@gmail.com>
2022-10-03 17:22:23 -0700

    add bento box page

---
+++ pages/bentobox.js
@@ -0,0 +1,18 @@
+import Styles from "../components/Theme";
+import Layout from "../components/Layout";
+import Spacer from "../components/Spacer";
+import Link from "next/link";
+
+export default function Index() {
+  return (
+    <div>
+      <Layout />
+      <div style={Styles.content}>
+        <h1 style={Styles.header}>Bento Wallet</h1>
+        Bento Box, or the Bento Wallet, is a project aimed to solve the problem of managing finances with new roommates in a new city.
+        
+
+      </div>
+    </div>
+  );
+}
